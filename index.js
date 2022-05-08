c32534c0e6807b17088c542b8311690f234c16ed
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-05-08 18:17:15 +0600

    change-collection

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 
-//const uri = "mongodb+srv://furniturehouse:7ngS5ABwkeOXQgHZ@cluster0.xcemb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xcemb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
