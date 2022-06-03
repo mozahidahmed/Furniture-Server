524b55acff87946181f673139d9904eb7848c936
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-06-03 19:13:08 +0600

    ccccccccccmmmmmppp

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 
-
+//mozahid mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xcemb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
